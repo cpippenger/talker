f5b516927cb4a54061d81784fdef9360f6299cca
cpippenger <cpippenger@gmail.com>
2024-02-12 05:35:06 -0500

    added create super chat insert
    - cleaned up app.py and added --install command line switch for db set up
    - added environment variables for database connectoin
    - added api call
    - added amount to superchat object
    - added install_deps.sh and index.html
    - created cache folder and template folder

--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
-
 import json
+import os,sys
 import logging
-import pandas as pd
-from uuid import uuid4
-from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4 
+from flask import Flask, jsonify, request, render_template
 import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -12,8 +12,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy import select
 from sqlalchemy.sql import text
-
-
+from datetime import datetime
 # User imports
 from models.super_chat import SuperChat
 
@@ -24,6 +23,7 @@ logging.basicConfig(
     format='[%(asctime)s] {%(lineno)d} %(levelname)s - %(message)s',
     datefmt='%H:%M:%S'
 )
+
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("speechbrain").setLevel(logging.WARNING)
@@ -31,85 +31,72 @@ logging.getLogger("espeakng").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+DATABASE_TYPE = os.environ.get('DATABASE_TYPE',"postgresql")
+DATABASE_USERNAME = os.environ.get('DATABASE_USER',"test")
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD',"test")
+DATABASE_SCHEMA = os.environ.get('DATABASE_SCHEMA',"chat")
+DATABASE_HOST = os.environ.get('DATABASE_HOST',"127.0.0.1")
+TTS_URL=os.environ.get('TTS_URL',"http://localhost:6666/api/tts?text=")
 
-DATABASE_TYPE = "postgresql"
-DATABASE_USERNAME = "test"
-DATABASE_PASSWORD = "test"
-DATABASE_SCHEMA = "chat"
-DATABASE_HOST = "192.168.1.4"
-
-app = Flask(__name__)
+app = Flask(__name__,static_folder="cache")
 app.config['SQLALCHEMY_DATABASE_URI'] = f'{DATABASE_TYPE}://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_SCHEMA}'
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
+SessionClass = sessionmaker(bind=engine)
+session = SessionClass()
 
-url = URL.create(
-    drivername=DATABASE_TYPE,
-    username=DATABASE_USERNAME,
-    password=DATABASE_PASSWORD,
-    host=DATABASE_HOST,
-    database=DATABASE_SCHEMA
-)
+# this will get replaced with real code later
+def save_tts(message,filename):
+    r = requests.get(TTS_URL + message, allow_redirects=True)
+    open(filename, 'wb').write(r.content)
+    return None
 
-engine = create_engine(url)
-Session = sessionmaker(bind=engine)
+def install():
+    try:
+        SuperChat.__table__.create(engine)
+    except sqlalchemy.exc.ProgrammingError:
+        raise
 
-def get_db_session():
-    return Session
-
-session = get_db_session()
-
-# Init tables
-try:
-    SuperChat.__table__.create(engine)
-except sqlalchemy.exc.ProgrammingError:
-    pass
-
-
-@app.get("/test")
+@app.get("/")
 async def root():
-    return {"message": "All good"}
+    return render_template('index.html')
 
-
-
-
-
+@app.route('/insert_super_chat', methods=['POST'])
+def insert_super_chats():
+    uuid=str(uuid4())
+    data = request.json
+    username=data.get('username')
+    text=data.get('text')
+    amount=data.get('amount')
+    if username != None and text != None and amount != None:
+        save_tts(username + " says " + text,"cache/" + uuid )
+        session.add(SuperChat(id=uuid,username=username,text=text,amount=amount,datetime_uploaded=datetime.now()))   
+        session.commit()
+        return '{"status":"success"}'
+    else:
+        return '{"status":"failed"}'
+        
 @app.route('/get_super_chats')
 def get_super_chats():
-
     statement = select(SuperChat)
-    logger.debug(f"{type(statement) = }")
-    logger.debug(f"{statement = }")
-    #user_obj = session.scalars(statement).all()
-
-    session = Session()
     rows = session.execute(statement).all()
-
     output = []
-
     if len(rows) == 0:
         logger.warning(f"get_super_chats(): No results found.")
-
     # For each row
     for row in rows:
-        #logger.debug(f"{type(row) = }")
-        #logger.debug(f"{row = }")
-        #logger.debug(f"{row._mapping = }")
         super_chat = row._mapping["SuperChat"]
-        #logger.debug(f"{type(aor) = }")
-        #logger.debug(f"{aor = }")
-        #logger.debug(f"{aor.to_dict() = }")
-        #output.append()
         output.append(super_chat.to_dict())
-
-    #df = pd.DataFrame.from_records(dict(zip(r.keys(), r)) for r in rows)
-
-    
     return json.dumps(output)
 
-
-
-# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--install":
+            print("Installing Database...")
+            install()
+        else:
+            print("Unrecognized Argument")
+            
+    else:
+        app.run(debug=True, host="0.0.0.0", port=5000)
--- api/models/super_chat.py
+++ api/models/super_chat.py
@@ -10,11 +10,12 @@ class SuperChat(Base):
     id = Column(UUID, primary_key=True, nullable=False, unique=True)
     username = Column(String(150), nullable=True, unique=False)
     text = Column(Text, nullable=True, unique=False)
+    amount=Column(String, nullable=True, unique=False)
     datetime_uploaded = Column(DateTime, nullable=True, unique=False)
-
+    
 
     def __str__(self):
-        return f'SuperChat(id="{str(self.id)}", username="{self.username}", text="{self.text}, datetime_uploaded="{str(self.datetime_uploaded)}'
+        return f'SuperChat(id="{str(self.id)}", username="{self.username}", text="{self.text}", amount="{self.amount}", datetime_uploaded="{str(self.datetime_uploaded)}"'
 
 
     def to_dict(self):
@@ -22,5 +23,6 @@ class SuperChat(Base):
             "id" : str(self.id),
             "username" : self.username,
             "text" : self.text,
+            "amount":self.amount,
             "datetime_uploaded" : str(self.datetime_uploaded)
         }
