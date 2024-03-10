7f986f34e081191784243d438e2ce452c88c5f5b
cpippenger <cpippenger@gmail.com>
2024-03-10 04:17:44 -0400

    - added buttons
    - added poster for video

--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
 import json
-import os,sys
+import os,sys,io
 import logging
 import requests
 from uuid import uuid4 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template,send_file
 import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -52,6 +52,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'{DATABASE_TYPE}://{DATABASE_USERNAME}:
 db = SQLAlchemy(app)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 SessionClass = sessionmaker(bind=engine)
+
+def web_get(url):
+    try:
+        r = requests.get(url )
+        r.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        return ("ERROR: " + str(err)) 
+    except requests.exceptions.HTTPError as errh:
+        return ("ERROR: swc_tts Http Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+        return ("ERROR: swc_tts Error Connecting:")
+    except requests.exceptions.Timeout as errt:
+        return ("ERROR: swc_tts Timeout Error:")
+    return r
+
 
 def web_post(url,payload):
     try:
@@ -103,8 +118,6 @@ def swc_tts(str_voice,str_message,str_filename):
     write(str_filename, int(r.json()["rate"]), scaled)
     return get_avatar_video(str_voice,str_filename)
 
-
-
 def eleven_tts (message,filename,voice):
     audio = generate(
           # api_key="YOUR_API_KEY", (Defaults to os.getenv(ELEVEN_API_KEY))
@@ -116,14 +129,11 @@ def eleven_tts (message,filename,voice):
 
 
 
-# - all endpoints must return path to tts file, like cache/<id>
-# - return string that starts with ERROR on error 
-# - can return an external url if it wants, but probably shouldnt, path
-#   is passed strait to audio src
-# - should be storing in /cache for playback
-# - prefered to prefix file name with static id of source e.g coqtts_<id>
-# - must be at least one end point
-endpoints={"trump": lambda str_message,str_filename: swc_tts("trump",str_message,str_filename)}
+
+endpoints={"trump" : {   "avatar" : web_get(SADTALKER_URL.replace('generate_avatar_message','view_system_face?name=trump')).content,
+                        "generator" : lambda str_message,str_filename: swc_tts("trump",str_message,str_filename) },
+            "dsp" : {   "avatar" : web_get(SADTALKER_URL.replace('generate_avatar_message','view_system_face?name=dsp')).content,
+                        "generator" : lambda str_message,str_filename: swc_tts("dsp",str_message,str_filename) }}
 
 current_endpoint="trump"# hardcoded for now, not sure what to do about this,
                             # was using the first of the array
@@ -144,6 +154,13 @@ async def clear_all():
     session.commit()
     session.close()
     return '<script>location.assign("/");</script>' #hack
+
+@app.route('/get_avatar', methods=['GET'])
+def get_avatar():
+    name=request.args.get('name')
+    if name == None or name not in endpoints.keys():
+        return "ERROR: Bad Avatar Name"
+    return send_file(io.BytesIO(endpoints[name]['avatar']),"application/octet-stream")
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -184,7 +201,7 @@ def insert_super_chats():
     amount=data.get('amount')
     if username != None and text != None and amount != None:
         #cache_file=save_tts(username + " says " + text,"cache/" + uuid )
-        cache_file=endpoints[current_endpoint](username + " says " + text,"cache/" + uuid )
+        cache_file=endpoints[current_endpoint]['generator'](username + " says " + text,"cache/" + uuid )
         session.add(SuperChat(id=uuid,username=username,text=current_endpoint + " - " +text,amount=amount,datetime_uploaded=datetime.now(),tts_file=cache_file))   
         session.commit()
         session.close()
